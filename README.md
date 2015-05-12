fd2beb1faa142013505cf61edc03052209f57e18
Evan You <yyx990803@gmail.com>
2015-05-12 11:00:38 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# ShowMe
+
+Small utility to open the github page for an NPM module.
+
+``` bash
+showme some-module
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var re = /'(git:|https:|git+https:)\/\/(github\.com\/.*?)(\.git)?'/
+var exec = require('child_process').exec
+var spawn = require('child_process').spawn
+var name = process.argv[2]
+
+exec('npm view ' + name, function (err, res) {
+  var match = res.match(re)
+  if (match) {
+    var url = match[2].replace(/\/issues$/, '')
+    openURL('https://' + url)
+  } else {
+    console.log('Can\'t find a github page for package: ' + name)
+  }
+})
+
+function openURL (url) {
+  switch (process.platform) {
+    case "darwin":
+      exec('open ' + url)
+      break
+    case "win32":
+      exec('start ' + url)
+      break
+    default:
+      spawn('xdg-open', [url])
+  }
+}
